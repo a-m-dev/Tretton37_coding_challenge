6717330222e50bf110437001964b26c8066e2fc8
a-m-dev <a.mirzaei.dev@gmail.com>
2021-02-10 00:28:27 +0330

    added google font

--- client/src/containers/EmployeesList/EmployeesList.jsx
+++ client/src/containers/EmployeesList/EmployeesList.jsx
@@ -3,7 +3,11 @@ import "./EmployeesList.scss";
 const EmployeeList = () => {
   return (
     <article className="employees-list">
-      <section className="container">EMPLOYEE LIST</section>
+      <section className="container">
+        <h1 className="employees-list__heading">
+          The fellowship of the tretton37
+        </h1>
+      </section>
     </article>
   );
 };
