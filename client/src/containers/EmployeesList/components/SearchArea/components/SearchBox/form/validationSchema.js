83643a2b04a8f1c92b947f564e717496c666da88
a-m-dev <a.mirzaei.dev@gmail.com>
2021-02-12 16:47:41 +0330

    validating serach field on wrong values entries

--- client/src/containers/EmployeesList/components/SearchArea/components/SearchBox/form/validationSchema.js
+++ client/src/containers/EmployeesList/components/SearchArea/components/SearchBox/form/validationSchema.js
@@ -1,7 +1,11 @@
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-  query: Yup.string().nullable().typeError("You must specify a valid query"),
+  query: Yup.string()
+    .typeError("You must specify a valid query")
+    .test("alphabets", "Name must only contain alphabets", (value) => {
+      return /^[A-Za-z]+$/.test(value);
+    }),
 });
 
 export default validationSchema;
